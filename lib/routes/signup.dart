7220c655e98cd1063fdc0e59491d652313e6f2b7
shankarkharel <59717384+shankarkharel@users.noreply.github.com>
2023-03-20 22:31:56 +0545

    changed signup feature

--- lib/routes/signup.dart
+++ lib/routes/signup.dart
@@ -200,7 +200,7 @@ class _SignUpPageState extends State<SignUpPage> {
                                 Navigator.pushReplacement(context,
                                     MaterialPageRoute(
                                   builder: (context) {
-                                    return HomePage();
+                                    return LoginPage();
                                   },
                                 ));
                               }
